e7edece95ef710ab46a407f3e0d72e8bccca016b
Lucas-brb <105973099+Lucas-brb@users.noreply.github.com>
2022-05-23 19:33:12 +0200

    Passage en (x,y) pour Zernike

--- Polynomes et Zernike/Projection sur Zernike.py
+++ Polynomes et Zernike/Projection sur Zernike.py
@@ -14,6 +14,6 @@ projection_zernike = []
 for n in range(6):
     pr = []
     for m in range(n+1):
-        pr.append(ps(Zernike(m,n,rho,phi),z1,[0,5],[0,5]))
+        pr.append(ps(Zernike_xy(m,n,x,y),z1,[0,5],[0,5]))
     projection_zernike.append(pr)
 print (projection_zernike)
--- Polynomes et Zernike/Zernike.py
+++ Polynomes et Zernike/Zernike.py
@@ -1,8 +1,8 @@
 import math
 
 def Rmn(m,n,rho):
+    R = 0
     if (n-m) % 2 == 0:
-        R = 0
         for k in range((n-m)/2+1):
             R += ((-1)**k*math.fact(n-k)) / (math.fact(k)*math.fact((n+m)/2-k)*math.fact((n-m)/2-k))*rho**(n-2*k)
     return R
@@ -12,3 +12,9 @@ def Zernike(m,n,rho,phi):
         return Rmn(m,n,rho)*math.cos(m*phi)
     else :
         return Rmn(m,n,rho)*math.sin(m*phi)
+
+def Zernike_xy(m,n,x,y):
+    if x < 0 and y == 0 :
+        return Zernike(m,n,math.sqrt(x*x+y*y),math.pi)
+    else :
+        return Zernike(m,n,math.sqrt(x*x+y*y),math.atan(y/(x+math.sqrt(x*x+y*y))))
